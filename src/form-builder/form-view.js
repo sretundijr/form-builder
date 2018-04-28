986587ace9706866ba828c9de73aa6afc4bebfff
sretundijr <sretundijr@gmail.com>
2018-04-28 15:36:49 -0600

    added form view component

---
+++ src/form-builder/form-view.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+// todo refactor condtional
+const renderElementList = (list) => {
+  if (list.length > 0) {
+    return list.map((item) => {
+      // todo add key
+      return (
+        <div>
+          <p>{item.label}</p>
+          <p>{item.selectedElement.type}</p>
+        </div>
+      );
+    })
+  }
+  return '';
+}
+
+const FormView = (props) => {
+  console.log(props.elementList)
+  return (
+    <div>
+      {renderElementList(props.elementList)}
+    </div>
+  )
+}
+
+export { FormView };
